65f897b75adea5f5bc1f19cc37a71a2417fc15f1
Yosu0515 <josdevries1989@live.nl>
2018-01-10 15:30:42 +0100

    Leave constants like this pl0x

--- IMDB_Parser/src/Constants/Constants.java
+++ IMDB_Parser/src/Constants/Constants.java
@@ -1,7 +1,7 @@
 package Constants;
 
 public class Constants {
-    public static final String dir = "C:\\Users\\Jan Julius\\Desktop\\Bigmoviedata\\";
+    public static final String dir = "C:\\data\\";
     public static final String csvDir = "out\\";
     public static final String[] data = {"actors.list", "actresses.list", "business.list", "countries.list", "genres.list", "movies.list", "ratings.list", "running-times.list"};
 
